739125687693108578bd222a19cb7d57ae7c8146
Kaushal Modi <kaushal.modi@gmail.com>
2021-05-07 12:34:01 -0400

    VPI binding via Nim works!
        xcelium> run
    
        Hello!!
    
        Simulation complete via $finish(1) at time 0 FS + 1
        ./tb.sv:7     $finish;

--- tests/basic/libvpi.nim
+++ tests/basic/libvpi.nim
@@ -1,8 +1,18 @@
 import svvpi
 
-proc hello() {.exportc, dynlib.} =
-  discard vpi_printf("\n\nHello!!\n\n")
+# Associate the `hello` Nim proc with a new System Task.
+proc dollarHello() =
+  # The proc needs to have the signature "proc (a1: cstring): cint {.cdecl.}"
+  # as that's what nimterop auto-parses the `t_vpi_systf_data.calltf` type to.
+  proc hello(s: cstring): cint {.cdecl.} =
+    discard vpi_printf("\n\nHello!!\n\n")
 
-proc foo() = echo "hi"
-proc bar() = echo "there"
-setVlogStartupRoutines(foo, bar)
+  var
+    taskDataObj = s_vpi_systf_data(`type`: vpiSysTask,
+                                   tfname: "$hello",
+                                   calltf: hello)
+
+  discard vpi_register_systf(addr taskDataObj)
+
+# Register the new system task.
+setVlogStartupRoutines(dollarHello)
