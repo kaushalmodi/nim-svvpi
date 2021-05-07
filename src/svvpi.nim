2e51181db9ff820d50098337076c020e7d1794a6
Kaushal Modi <kaushal.modi@gmail.com>
2021-05-07 15:01:04 -0400

    Add vpiRegisterTask macro

--- src/svvpi.nim
+++ src/svvpi.nim
@@ -1,4 +1,4 @@
-import std/[os]
+import std/[os, macros, strutils]
 import nimterop/cimport
 
 when defined(svGenWrapper):
@@ -64,3 +64,29 @@ template setVlogStartupRoutines*(procArray: varargs[proc() {.nimcall.}]) =
   for i in 0 ..< numProcs:
     vlog_startup_routines[i] = procArray[i]
   vlog_startup_routines[numProcs] = nil
+
+macro vpiRegisterTask*(procName: static string; body: untyped) =
+  if procName.len <= 1:
+    echo "[Error] The proc sym needs to be at least 2 chars long."
+    quit QuitFailure
+
+  let
+    procSym = ident(procName)
+    # procSym = foo -> wrapperProcSym = dollarFoo
+    wrapperProcSym = ident("dollar" & procName[0].toUpperAscii() & procName[1 .. procName.high])
+
+  result = quote do:
+    proc `wrapperProcSym`() =
+      # Below proc needs to have the signature "proc (a1: cstring): cint
+      # {.cdecl.}"  as that's what nimterop auto-parses the
+      # `t_vpi_systf_data.calltf` type to.
+      proc `procSym`(s: cstring): cint {.cdecl.} =
+        `body`
+
+      var
+        taskDataObj = s_vpi_systf_data(type: vpiSysTask,
+                                       tfname: "$" & `procName`,
+                                       calltf: `procSym`,
+                                       compiletf: nil)
+
+      discard vpi_register_systf(addr taskDataObj)
--- tests/basic/libvpi.nim
+++ tests/basic/libvpi.nim
@@ -1,18 +1,7 @@
 import svvpi
 
-# Associate the `hello` Nim proc with a new System Task.
-proc dollarHello() =
-  # The proc needs to have the signature "proc (a1: cstring): cint {.cdecl.}"
-  # as that's what nimterop auto-parses the `t_vpi_systf_data.calltf` type to.
-  proc hello(s: cstring): cint {.cdecl.} =
-    discard vpi_printf("\n\nHello!!\n\n")
-
-  var
-    taskDataObj = s_vpi_systf_data(`type`: vpiSysTask,
-                                   tfname: "$hello",
-                                   calltf: hello)
-
-  discard vpi_register_systf(addr taskDataObj)
+vpiRegisterTask "hello":
+  discard vpi_printf("\n\nHello!!\n\n")
 
 # Register the new system task.
 setVlogStartupRoutines(dollarHello)
