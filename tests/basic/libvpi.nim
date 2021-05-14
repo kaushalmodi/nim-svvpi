70c7322d193a4e7f5b3af2f40fd79256e2f17e1d
Kaushal Modi <kaushal.modi@gmail.com>
2021-05-13 21:02:12 -0400

    Add a dummy $do_nothing vpi sys function

--- tests/basic/libvpi.nim
+++ tests/basic/libvpi.nim
@@ -8,5 +8,10 @@ vpiDefine task bye:
   calltf:
     vpiEcho("Bye!")
 
+vpiDefine function do_nothing:
+  compiletf: discard
+  calltf: discard
+  sizetf: discard
+
 # Register the tasks.
-setVlogStartupRoutines(hello, bye)
+setVlogStartupRoutines(hello, bye, do_nothing)
--- tests/basic/tb.sv
+++ tests/basic/tb.sv
@@ -1,11 +1,11 @@
-// Time-stamp: <2021-05-07 16:34:15 kmodi>
+// Time-stamp: <2021-05-13 21:01:49 kmodi>
 
 program top;
 
   initial begin
     $hello;
     $bye;
-
+    void'($do_nothing);
     $finish;
   end
 
